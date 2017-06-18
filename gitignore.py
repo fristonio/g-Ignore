910e337a4afeb37d599b3281d3c403878234e92d
Deepesh Pathak <deepshpathak@gmail.com>
2017-06-18 20:07:22 +0530

    Add gitIgnore.py to scrape standard gitignore from github/gitignore

--- gitignore.py
+++ gitignore.py
@@ -1,10 +1,33 @@
-import requests
+#!/usr/bin/python3
 
-GITIGNORE_URL = "https://github.com/github/gitignore"
+import requests
+import os
+import json
+
+
+BASE_URL = "https://raw.githubusercontent.com/github/gitignore/master/%s.gitignore"
+APP_PATH = os.environ['HOME'] + "/.gIgnore/"
 
 
 def main():
-    requests.get()
+    try:
+        os.makedirs(APP_PATH)
+    except:
+        print("[-] Home directory exists")
+
+    with open("gitIgnore-lang.json", "r") as file:
+        langJson = json.load(file)
+        for language in langJson:
+            lang = list(language.keys())[0]
+            try:
+                langPath = APP_PATH + lang + '.gitignore'
+                res = requests.get(BASE_URL % lang)
+                with open(langPath, "w") as gigFile:
+                    gigFile.write(res.text)
+                    print("[+] Updated %s.gitignore" % lang)
+
+            except:
+                print("[-] File Already Exist")
 
 
 if __name__ == "__main__":
