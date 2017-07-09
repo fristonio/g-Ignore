da16c045834b26253b70d10acfb17824965e14a3
Deepesh Pathak <deepshpathak@gmail.com>
2017-07-09 16:16:16 +0530

    update gIgnore.py to create gitignore

--- gIgnore.py
+++ gIgnore.py
@@ -2,64 +2,106 @@
 
 import os
 import json
+import re
+import fnmatch
 
 
 APP_PATH = os.environ['HOME'] + "/.gIgnore/"
 
 
-def createGitignore(files):
-    # Language extensions for the directory.
+def removeDuplicate():
+    """
+    Remove duplicate file matching from gitignore files
+    """
+    seen = []
+    with open(".gitignore", "r") as gitIgn:
+        for file in gitIgn.read().split('\n'):
+            if file not in seen:
+                seen.append(file)
+    with open(".gitignore", "w") as gitIgn:
+        for file in seen:
+            gitIgn.write(file + '\n')
+
+
+def refineGitignore(contents, files):
+    refinedContents = ""
+    for exp in contents.split('\n'):
+        for file in files:
+            if fnmatch.fnmatch(file, exp):
+                refinedContents += exp + '\n'
+                break
+    return refinedContents
+
+
+def createGitignore(gitIgnoreLangs, files):
+    with open(".gitignore", "w") as gitIgn:
+        for lang in gitIgnoreLangs:
+            gitignoreContent = ""
+            path = APP_PATH + lang + ".gitignore"
+            try:
+                with open(path, "r") as f:
+                    gitignoreContent += refineGitignore(f.read(), files)
+                    gitIgn.write(gitignoreContent)
+            except:
+                pass
+    removeDuplicate()
+
+
+def getLangs(files):
     allExt = []
     for file in files:
-        file = file.split('.')
-        fileExt = '.' + file[-1]
+        fileExt = '.' + file.split('.')[-1]
         if fileExt not in allExt:
             allExt.append(fileExt)
-    print(allExt)
 
-    allLangs = []
-    with open("gitIgnore-lang.json", "r") as f:
+    gitIgnoreLangs = []
+    with open(APP_PATH + "langs.json", "r") as f:
         langJson = json.load(f)
         for ext in allExt:
             for lang, exts in langJson.items():
                 if ext in exts:
-                    allLangs.append(lang)
+                    gitIgnoreLangs.append(lang)
                     break
-    print(allLangs)
-
-    gitignoreContent = ""
-
-    for lang in allLangs:
-        path = APP_PATH + lang + ".gitignore"
-        try:
-            with open(path, "r") as f:
-                gitignoreContent += f.read()
-        except:
-            pass
-
-    print(gitignoreContent)
-    with open(".gitignore", "w") as f:
-        f.write(gitignoreContent)
+    return gitIgnoreLangs
 
 
-def traverseDirectory():
+def checkIfGitRepo():
+    files = os.listdir(os.getcwd())
+    if '.git' in files:
+        return True
+    return False
+
+
+def traverseDirectory(cwd):
+    """
+    Traverses current directory and returns a list of files and dirs present,
+    skips hidden directories
+    """
+    gitRepo = checkIfGitRepo()
     allFiles = []
-    for (curDir, dirs, files) in os.walk(os.getcwd()):
-        if ".git" not in curDir:
-            for file in files:
-                allFiles.append(curDir + '/' + file)
-    print(allFiles)
-    createGitignore(allFiles)
+    allDirs = []
+    if gitRepo:
+        for (curDir, dirs, files) in os.walk(cwd):
+            try:
+                re.search(r'/(\.\w+)', curDir).group(0)
+            except:
+                allDirs = allDirs + dirs
+                allFiles = allFiles + files
+        return allFiles, allDirs
+    else:
+        print("[-] Directory is not a git repository") 
 
 
 def main():
-    try:
-        os.makedirs(APP_PATH)
-    except:
-        print("[-] Home directory exists")
-
-    traverseDirectory()
+    allFiles, allDirs = traverseDirectory(os.getcwd())
+    gitIgnoreLangs = getLangs(allFiles)
+    data = allDirs + allFiles
+    createGitignore(gitIgnoreLangs, data)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        os.stat(APP_PATH)
+        main()
+    except Exception as e:
+        print(e)
